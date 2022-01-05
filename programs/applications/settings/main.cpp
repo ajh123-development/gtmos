54ed11973799a6e5092cf0d02e5cf890a6a93719
ajh123 <ajh123@users.noreply.github.com>
2022-01-05 15:34:47 +0000

    more Settings App fix 4

--- programs/applications/settings/main.cpp
+++ programs/applications/settings/main.cpp
@@ -33,7 +33,8 @@ int main(int argc, char** argv, char** envp) {
 	auto window = UI::Window::create();
 
 	const Duck::Path path = _app_info.base_path();
-	const char* path_str = path.string().c_str()+"/pages";
+	std::string path_sstr = path_str = path.string() + "/pages";
+	const char* path_str = path_sstr.c_str();
 	Duck::Log::info("Settings view at ", path_str);
 
 	auto dir_widget = SettingsViewWidget::make(path_str);
