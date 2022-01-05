3d0ac6ac6b6faa72c1705a4accf2f2b1e837e211
ajh123 <ajh123@users.noreply.github.com>
2022-01-05 17:09:09 +0000

    more Settings App fix 5

--- programs/applications/settings/SettingsViewWidget.cpp
+++ programs/applications/settings/SettingsViewWidget.cpp
@@ -33,12 +33,14 @@ SettingsViewWidget::SettingsViewWidget(const Duck::Path& path): ListView(GRID) {
 }
 
 Widget::Ptr SettingsViewWidget::create_entry(int index) {
-	if((!index) && path.parent().string() != root.string()) {
-		auto btn = UI::Button::make("<---");
-		btn->on_released = [&] {
-			set_directory(path.parent());
-			return true;
-		};
+	if(!index) {
+		if (path.parent().string() != root.string()) {
+			auto btn = UI::Button::make("<---");
+			btn->on_released = [&] {
+				set_directory(path.parent());
+				return true;
+			};
+		}
 		return btn;
 	}
 
