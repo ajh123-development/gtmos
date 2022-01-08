75dd20fedc0bde38a11dd9200e0d13a1426a8bbe
ajh123 <ajh123@users.noreply.github.com>
2022-01-08 14:48:30 +0000

    A few name changes

--- services/init/main.cpp
+++ services/init/main.cpp
@@ -12,18 +12,18 @@
 /*
     This file is part of duckOS.
     
-    duckOS is free software: you can redistribute it and/or modify
+    duckOS and GTMOS is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
     
-    duckOS is distributed in the hope that it will be useful,
+    duckOS and GTMOS is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     
     You should have received a copy of the GNU General Public License
-    along with duckOS.  If not, see <https://www.gnu.org/licenses/>.
+    along with duckOS and GTMOS.  If not, see <https://www.gnu.org/licenses/>.
     
     Copyright (c) Byteduck 2016-2020. All rights reserved.
 */
@@ -41,7 +41,7 @@ int main(int argc, char** argv, char** envp) {
 	}
 
 	setsid();
-	Log::success("Welcome to duckOS!");
+	Log::success("Welcome to GTMOS!");
 
 	//Read config file
 	auto cfg_res = Config::read_from("/etc/init.conf");
