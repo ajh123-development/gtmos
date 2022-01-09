aa7580802f96703d039fb1e8f5ebaf3a21d4d641
ajh123 <ajh123@users.noreply.github.com>
2022-01-09 18:39:39 +0000

    More logon app 42

--- programs/applications/logon/LogonWidget.cpp
+++ programs/applications/logon/LogonWidget.cpp
@@ -36,8 +36,10 @@ LogonWidget::LogonWidget(UI::Window::Ptr& window): BoxLayout(VERTICAL) {
 bool LogonWidget::on_keyboard(Pond::KeyEvent evt){
 	bool control = KBD_MOD_CTRL & evt.modifiers;
 	bool alt = KBD_MOD_ALT & evt.modifiers;
-	if(control && alt && evt.key == 46){
-		Log::info("Control Alt Delete");
+	if(control && alt &&){
+		if(evt.scancode == 46){
+			Log::info("Control Alt Delete");
+		}
 	}
 	return false;
 }
