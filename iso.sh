b8e442423ec63d278ff8e1f1283d9a39d21c4721
ajh123 <ajh123@users.noreply.github.com>
2023-02-14 19:15:02 +0000

    GTMOSify

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/gtmos.kernel isodir/boot/gtmos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "gtmos" {
+	multiboot /boot/gtmos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o gtmos.iso isodir
