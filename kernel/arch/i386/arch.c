a070f86f9fbd1056cea61177c24eb93d66c31a78
ajh123 <ajh123@users.noreply.github.com>
2023-02-16 18:09:31 +0000

    The GDT

--- kernel/arch/i386/arch.c
+++ kernel/arch/i386/arch.c
@@ -1,7 +1,9 @@
 #include <kernel/arch/arch.h>
 #include <gtmos/logging.h>
+#include "gtd.h"
 
 int arch_init() {
+    loadGDT();
     log_ok("CPU", "The i386 subsytem has loaded corectly!");
     return 0;
 }
